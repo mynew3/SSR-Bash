2223b9ad831a01f389bd4ea8250f090802159745
YLWS-4617 <wmy461737664@qq.com>
2016-12-11 22:28:12 -0500

    Add useful functions.

--- install.sh
+++ install.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-######Check Root######
+
+#Check Root
 [ $(id -u) != "0" ] && { echo "${CFAILURE}Error: You must be root to run this script${CEND}"; exit 1; }
-######Check Root#####
+
+#Set DNS
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+
+#Disable selinux
+if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; then
+    sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+    setenforce 0
+fi
+
 clear
 
 #InstallBasicPackages
 apt-get update -y
-apt-get install build-essential git vim tar python-pip unzip bc -y
+apt-get install git tar python-pip unzip bc python-m2crypto curl wget unzip gcc swig automake make perl cpio build-essential ntpdate -y
 apt-get install language-pack-zh-hans -y
 pip install shadowsocks
+
+#Set Time Zone
+rm -rf /etc/localtime
+ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+ntpdate 1.cn.pool.ntp.org
 
 #Clone Something
 cd /usr/local
